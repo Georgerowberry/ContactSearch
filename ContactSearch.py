79a99a2f7f310810b24f45987e4984bf9047c497
george <george.rowberry@gmail.com>
2018-03-16 17:54:02 +0000

    Finished Product

--- ContactSearch.py
+++ ContactSearch.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, request
 import json
 import os
 
@@ -8,13 +9,51 @@ file_path = project_dir + "/data.json"
 app = Flask(__name__)
 
 
-@app.route('/')
-def start():
+def get_data():
     with open(file_path, 'r') as file:
         data = json.load(file)
-    print(data)
+    data = sorted(data, key=lambda x: x['name'])
+    return data
 
-    return render_template('base.html', data=data)
+
+def search_data(data, search_term):
+    """
+    :param data: list of dicts of contacts
+    :param search_term: string to search for
+    :return: dict of contacts matching search term
+    """
+    matches = []
+
+    def find_match(value):
+        if search_term.lower() in value.lower() or search_term.lower() == value.lower():
+            if d not in matches:
+                matches.append(d)
+
+    for d in data:
+        for k, v in d.items():
+            if isinstance(v, list):
+                for i in v:
+                    find_match(i)
+            else:
+                find_match(v)
+
+    return matches
+
+
+@app.route('/')
+def start():
+    search = request.args.get('search')
+    data = get_data()
+    if search:
+        print(search)
+        matching_contacts = search_data(data, search)
+        context = {'results': True, 'matches': matching_contacts, 'search_term': search}
+        return render_template('results.html', data=context)
+    elif search == '':
+        context = {'results': True, 'matches': data, 'search_term': search}
+        return render_template('results.html', data=context)
+    else:
+        return render_template('search.html', data=data)
 
 
 if __name__ == '__main__':
